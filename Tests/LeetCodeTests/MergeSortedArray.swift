15d8af3ac0e8b6c9658716c2d2afbd73bc01fc0b
keith27staines <keith27staines@icloud.com>
2024-02-05 20:20:34 +0000

    Changed signature to match leetcode
    passes leetcode

--- Tests/LeetCodeTests/MergeSortedArray.swift
+++ Tests/LeetCodeTests/MergeSortedArray.swift
@@ -16,47 +16,40 @@ final class MergeSortedArray: XCTestCase {
         // [1,2,3,0,0,0], m = 3, nums2 = [2,5,6], n = 3
         a = [1,2,3,0,0,0]
         b = [2,5,6]
-        merge(a: &a, m: 3, b: b, n: 3)
+        merge(&a, 3, b, 3)
         XCTAssertEqual(a, [1,2,2,3,5,6])
         
         // nums1 = [1], m = 1, nums2 = [], n = 0
         a = [1]
         b = []
-        merge(a: &a, m: 1, b: b, n: 0)
+        merge(&a, 1, b, 0)
         XCTAssertEqual(a, [1])
         
         // Input: nums1 = [0], m = 0, nums2 = [1], n = 1
         a = [0]
         b = [1]
-        merge(a: &a, m: 0, b: b, n: 1)
+        merge(&a, 0, b, 1)
         XCTAssertEqual(a, [1])
-        
-        
     }
 
 }
 
-/*
- [1 ,2 ,3, 0, 0, 0]
- [2, 5, 6]
- */
-
-func merge(a: inout [Int], m: Int, b: [Int], n: Int) {
-    let c = a
+func merge(_ nums1: inout [Int], _ m: Int, _ nums2: [Int], _ n: Int){
+    let c = nums1
     var i = 0
     var j = 0
     
     for k in 0..<m+n {
         if j < n {
-            if c[i] <= b[j] && i < m {
-                a[k] = c[i]
+            if c[i] <= nums2[j] && i < m {
+                nums1[k] = c[i]
                 i += 1
             } else {
-                a[k] = b[j]
+                nums1[k] = nums2[j]
                 j += 1
             }
         } else {
-            a[k] = c[i]
+            nums1[k] = c[i]
             i += 1
         }
     }
